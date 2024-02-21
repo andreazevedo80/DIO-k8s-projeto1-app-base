a4aadbb2012a43882e2284fd50a79893cb3b4463
andreazevedo80 <andre.l.azevedo69@gmail.com>
2024-02-21 10:25:38 -0300

    subindo o projeto

---
+++ script.bat
@@ -0,0 +1,19 @@
+echo "Criando as imangens..."
+
+docker build -t andreazevedo80/projeto-backend:1.0 backend/.
+docker build -t andreazevedo80/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens..."
+
+docker push andreazevedo80/projeto-backend:1.0
+docker push andreazevedo80/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernets.."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments..."
+
+kubectl apply -f ./deployments.yml
+
+
